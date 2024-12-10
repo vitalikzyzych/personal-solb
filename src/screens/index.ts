759297087c108d7cf67e37077d5090b997fc4c1c
vitalikzyzych <vitalikzyzych@gmail.com>
2024-12-10 10:40:52 +0200

    Update dir name

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,7 +2,7 @@ import Dashboard from "./dashboard";
 import ChartSandbox from "./chart-sandbox";
 import Login from "./login";
 import Settings from "./settings";
-import StakeHolderFieldPage from "./analysis/stakeholderField2";
+import StakeHolderFieldPage from "./analysis/stakeholderField";
 import Stakeholders from "./stakeholders/Stakeholders";
 import StakeholderView from "./stakeholders/StakeholderView";
 import Values from "./data-review/values";
